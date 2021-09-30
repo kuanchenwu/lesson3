fa5d2e79eaea554717a54bb0c2bf15aeb14a4037
Kuan Chen Wu <kwu3@uco.edu>
2021-09-29 20:46:15 -0500

    let's begin

---
+++ model/Question.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Question{
+	
+	private String text; // question text
+	private String answer;
+
+	public Question(String text, String answer){
+		this.text = text;
+		this.answer = answer;
+	}
+
+	public boolean checkAnswer(String response){
+		return response.equalsIgnoreCase(answer);
+	}
+
+	public void display(){
+		System.out.println(text);
+	}
+}
