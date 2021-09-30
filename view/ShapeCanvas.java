7361d0bcd1a0c6ea6a3a1d4d2ea799bbf7f951cd
Kuan Chen Wu <kwu3@uco.edu>
2021-09-30 14:36:20 -0500

    done

--- view/ShapeCanvas.java
+++ view/ShapeCanvas.java
@@ -37,4 +37,7 @@ public class ShapeCanvas extends JPanel{
 		}
 	}
 	
+	public ArrayList<Shape> getShapes(){
+		return shapes;
+	}
 }
--- view/ShapePanel.java
+++ view/ShapePanel.java
@@ -7,11 +7,14 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import controller.ShapeEventListener;
+
 public class ShapePanel {
 
 	private JFrame window;
 	private ShapeCanvas canvas;
-
+	private JButton randomShapeButton = new JButton("Random Shapes");
+	private JButton exitButton = new JButton("Exit");
 
 	public ShapePanel(JFrame window){
 		this.window = window;
@@ -22,14 +25,33 @@ public class ShapePanel {
 		JPanel southPanel = new JPanel();
 		cp.add(BorderLayout.SOUTH, southPanel);
 
-		JButton randomShapeButton = new JButton("Random Shapes");
-		JButton exitButton = new JButton("Exit");
+		
 		southPanel.add(randomShapeButton);
 		southPanel.add(exitButton);
 		
 		//canvas
 		canvas = new ShapeCanvas(this);
 		cp.add(BorderLayout.CENTER, canvas);
+
+		ShapeEventListener listener = new ShapeEventListener(this);
+		randomShapeButton.addActionListener(listener);
+		exitButton.addActionListener(listener);
+	
+	}
+
+	public JButton getExitButton() {
+		return exitButton;
 	}
 	
+	public JButton getRandomShapeButton() {
+		return randomShapeButton;
+	}
+
+	public ShapeCanvas getCanvas(){
+		return canvas;
+	}
+
+	public JFrame getWindow() {
+		return window;
+	}
 }
